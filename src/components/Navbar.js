703468cb042b44662eca3a9cc322395bb1698ca8
Srigowri <srigowri.venkatesha@gmail.com>
2022-05-22 20:08:54 +0530

    check navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,68 @@ import React, {useState} from 'react';
 
 import { Link } from 'react-router-dom';
 import { Navbar, NavDropdown, Nav, Container } from 'react-bootstrap';
+import { css } from '@emotion/css'
+import { ethers } from 'ethers'
+import 'easymde/dist/easymde.min.css'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { AccountContext } from '../context.js'
+
 
 const NavbarComp = () =>{
+    const [account, setAccount] = useState(null)
+    async function getWeb3Modal() {
+      const web3Modal = new Web3Modal({
+        network: 'mainnet',
+        cacheProvider: false,
+        providerOptions: {
+          walletconnect: {
+            package: WalletConnectProvider,
+            options: { 
+              // infuraId: 
+              infuraId: '9282b715ab934b08bc2e6eacf20e889f'
+            },
+          },
+        },
+      })
+      return web3Modal
+    }
+    async function connect() {
+      try {
+        const web3Modal = await getWeb3Modal()
+        const connection = await web3Modal.connect()
+        const provider = new ethers.providers.Web3Provider(connection)
+        const accounts = await provider.listAccounts()
+        setAccount(accounts[0])
+        console.log(accounts)  
+        
+
+      } catch (err) {
+        console.log('error:', err)
+      }
+    }
+
     return(
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
         <Navbar.Brand href="#home">NFT-Futures</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
+          {
+            !account && (          
+              <Nav>
+              {/* <Nav.Link href="/login">Login</Nav.Link> */}
+              <button onClick={connect}>Connect Wallet</button>  
+              </Nav>)
+          }
           <Nav className="me-auto">
-            <Nav.Link href="/upcomingbets">Upcoming Bets</Nav.Link>
-            <Nav.Link href="/history">History</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">Login</Nav.Link>
-          </Nav>
+            <Nav.Link href="/upcomingbets">{account && <p> Upcoming Bets </p>}</Nav.Link>
+            <Nav.Link href="/history">{account && <p>History</p>}</Nav.Link>
+          </Nav>                   
+          {
+            account && <p className={accountInfo}>{account}</p>
+          }          
+
         </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -27,7 +74,7 @@ export default NavbarComp;
 
 
 
-<Navbar bg="light" expand="lg">
+{/* <Navbar bg="light" expand="lg">
 <Container>
     <Navbar.Brand href="#home">NFT-Futures</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -42,4 +89,13 @@ export default NavbarComp;
         
     </Navbar.Collapse>
 </Container>
-</Navbar>
+</Navbar> */}
+
+const accountInfo = css`
+  width: 100%;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  font-size: 14px;
+  color: white;
+`
